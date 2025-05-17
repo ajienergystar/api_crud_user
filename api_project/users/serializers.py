55e007edda1d0d40197a5e61f7b4f70b7555113f
macAji <ajienergystar@gmail.com>
2025-05-17 23:23:45 +0700

    Upload All Source Code

---
+++ api_project/users/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'phone', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
---
+++ api_project/users/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets, permissions
+from .serializers import UserSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
